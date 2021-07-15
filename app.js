d87853878eab2808976ac0aca14eaa3b600125ec
oliver <olivier.codes@gmail.com>
2021-07-15 16:10:13 +0200

    fix bugs

--- app.js
+++ app.js
@@ -54,12 +54,13 @@ app.put('/api/pokemons/:id', (req, res) => {
   res.json(success(message, pokemonUpdated))
 });
 // route pour supprimer un pokemon
-app.delete('/api/pokemons/:id', (req, res) =>{
+app.delete('/api/pokemons/:id', (req, res) => {
   const id = parseInt(req.params.id)
-  const pokemonDeleted = {...req.body, id:id}
-  const message = `Le pokemon ${pokemonDeleted.name} a bien été supprimé.`
+  const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+  pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+  const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
   res.json(success(message, pokemonDeleted))
-})
+});
 
 //## server ##
 app.listen(port, () => {
